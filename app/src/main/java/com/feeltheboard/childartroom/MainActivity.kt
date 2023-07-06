82065903fb3e286a4a9adeab0c0e002911e78e99
rafaover <rafamsilva@gmail.com>
2023-07-06 14:35:07 +1000

    MainActivity.kt: - Added MainSliderLayout. Basic layout to start.

--- app/src/main/java/com/feeltheboard/childartroom/MainActivity.kt
+++ app/src/main/java/com/feeltheboard/childartroom/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.feeltheboard.childartroom
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.feeltheboard.childartroom.ui.theme.ChildArtRoomTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ChildArtRoomTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainSliderLayout()
                 }
             }
         }
@@ -30,17 +39,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainSliderLayout() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth(),
+            color = MaterialTheme.colorScheme.surface,
+            shape = MaterialTheme.shapes.large
+        ) {
+            // Image(painter = TODO(), contentDescription = null)
+            Text(text = "Child's Image")
+        }
+        Spacer(modifier = Modifier.padding(bottom = 30.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                // Icon(painter = TODO(), contentDescription = null)
+                Text(text = "Previous")
+            }
+            Text(text = "Name of the Drawing")
+            Button(onClick = { /*TODO*/ }) {
+                // Icon(painter = TODO(), contentDescription =null)
+                Text(text = "Next")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainActivityPreview() {
     ChildArtRoomTheme {
-        Greeting("Android")
+        MainSliderLayout()
     }
 }
