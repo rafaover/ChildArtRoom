4d6ada7d2ff92a54c6198e177170aac7a87dd654
rafaover <rafamsilva@gmail.com>
2023-07-28 11:49:47 +1000

    MainActivity.kt: - Added Image to surface at top

--- app/src/main/java/com/feeltheboard/childartroom/MainActivity.kt
+++ app/src/main/java/com/feeltheboard/childartroom/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.feeltheboard.childartroom
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,7 +56,11 @@ fun MainSliderLayout() {
             shape = MaterialTheme.shapes.large,
         ) {
             // Image from Database
-            // Image(painter = TODO(), contentDescription = null)
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Image",
+                modifier = Modifier.fillMaxWidth()
+            )
         }
         Spacer(modifier = Modifier.padding(bottom = 30.dp))
         Row(modifier = Modifier.fillMaxWidth()) {
